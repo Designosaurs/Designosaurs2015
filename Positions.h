066d9dfc479580a947bf759aa621927b2e0f2cde
rteder <rteder@hydreon.com>
2013-10-11 17:19:32 -0500

    Position Tweaks

--- Positions.h
+++ Positions.h
@@ -5,7 +5,7 @@
 |----/ \__  ---/ | __/  |  | \--/ ---/ \--\ \--\ |   ---/  Team 6369
 */
 ////////////////  POSITIONS TABLE
-#define PLACE_BLCCKS_LOW_SHOUDLER -65
+#define PLACE_BLOCKS_LOW_SHOUDLER -65
 #define	PLACE_BLOCKS_LOW_ELBOW -101
 
 #define PLACE_BLOCKS_MID_SHOUDLER -65
@@ -17,11 +17,11 @@
 #define GET_BLOCKS_SHOULDER -89
 #define	GET_BLOCKS_ELBOW -72
 
-#define RCLAW_OPEN 80
-#define RCLAW_CLOSED 20
+#define RCLAW_OPEN 40
+#define RCLAW_CLOSED 110
 
 #define LCLAW_OPEN 180
-#define LCLAW_CLOSED 140
+#define LCLAW_CLOSED 120
 
 #define TRAVEL_GET_SHOULDER 18
 #define	TRAVEL_GET_ELBOW -105
