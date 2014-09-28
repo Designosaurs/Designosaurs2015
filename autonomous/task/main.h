1b38b7b30d9de93c6e6d5eaf24472c1774fa5d54
williamtdr <william@cboos.com>
2014-09-28 17:16:03 -0500

    Reflect motors

--- autonomous/task/main.h
+++ autonomous/task/main.h
@@ -1,6 +1,8 @@
 task main() {
     waitForStart();
 
+    bMotorReflected[left_drive] = true;
+
     nMotorEncoder[left_drive] = 0;
 
     StartTask(DebugTask);
@@ -8,7 +10,7 @@ task main() {
 
     accel(0,100);
     goForwardDistance(3,100);
-    accel(100,20);
-    goForwardDistance(5,100);
+    accel(100,5);
+    goForwardDistance(5,5);
     stop();
 }
