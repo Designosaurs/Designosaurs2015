8d6304b71f16cb48a89de1225dc391695301ac06
williamtdr <william@cboos.com>
2014-09-28 14:15:26 -0500

    Autonomous Initial Commit

---
+++ autonomous/main.c
@@ -0,0 +1,25 @@
+#pragma config(Hubs,  S1, HTMotor,  HTServo,  none,     none)
+#pragma config(Motor,  mtr_S1_C1_1,     ,              tmotorNone, openLoop)
+#pragma config(Motor,  mtr_S1_C1_2,     ,              tmotorNone, openLoop)
+
+/*
+  _____            _
+ |  __ \          (_)
+ | |  | | ___  ___ _  __ _ _ __   ___  ___  __ _ _   _ _ __ ___
+ | |  | |/ _ \/ __| |/ _` | '_ \ / _ \/ __|/ _` | | | | '__/ __|
+ | |__| |  __/\__ \ | (_| | | | | (_) \__ \ (_| | |_| | |  \__ \
+ |_____/ \___||___/_|\__, |_| |_|\___/|___/\__,_|\__,_|_|  |___/
+                      __/ |
+                     |___/                            Team #6369
+ Autonomous Code: 2014-2015 (Cascade Effect)
+ */
+
+#include "JoystickDriver.c"
+
+task main() {
+    waitForStart();
+
+    while (true) {
+
+    }
+}
