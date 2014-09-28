fbe5d8edbadd166744e32f76bd1e1d9eaad75560
williamtdr <william@cboos.com>
2014-09-28 16:54:01 -0500

    Tweak encoder setup

--- autonomous/system/vars.h
+++ autonomous/system/vars.h
@@ -3,5 +3,5 @@ int last_power = 0;
 int left_encoder = 0;
 int prev_left_encoder = 0;
 
-int distance_counts = 0;
-int distance_feet = 0;
+long distance_counts = 0;
+float distance_feet = 0;
--- autonomous/task/UpdateEncoderTask.h
+++ autonomous/task/UpdateEncoderTask.h
@@ -1,11 +1,13 @@
 task UpdateEncoderTask() {
-    left_encoder = nMotorEncoder[left_drive];
+    while(true) {
+        left_encoder = nMotorEncoder[left_drive];
 
-    distance_counts += (left_encoder - prev_left_encoder);
-    distance_feet = distance_counts / COUNTS_PER_FOOT;
+        distance_counts += (left_encoder - prev_left_encoder);
+        distance_feet = (float) distance_counts / COUNTS_PER_FOOT;
 
-    writeDebugStreamLine("Left encoder is currently at (feet): %d",distance_feet);
+        writeDebugStreamLine("Left encoder is currently at (feet): %d",distance_feet);
 
-    prev_left_encoder = left_encoder;
-    wait1Msec(20);
+        prev_left_encoder = left_encoder;
+        wait1Msec(20);
+    }
 }
