01a079f87c70144ad19a33775a8f375caf0808cd
williamtdr <william@cboos.com>
2014-09-28 17:07:57 -0500

    Structure for autonomous

--- autonomous/task/main.h
+++ autonomous/task/main.h
@@ -6,10 +6,9 @@ task main() {
     StartTask(DebugTask);
     StartTask(UpdateEncoderTask);
 
-    while (true) {
-        accel(0,100);
-        wait1Msec(3000);
-        accel(100,0);
-        wait1Msec(6000);
-    }
+    accel(0,100);
+    goForwardDistance(3,100);
+    accel(100,20);
+    goForwardDistance(5,100);
+    stop();
 }
