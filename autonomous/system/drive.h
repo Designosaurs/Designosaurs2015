52c842f46817f0957d66c647517c54af9e38c8c2
williamtdr <william@cboos.com>
2014-09-28 14:52:10 -0500

    accel(), decel()

--- autonomous/system/drive.h
+++ autonomous/system/drive.h
@@ -15,9 +15,13 @@ void goBackwardDistance(float feet, int power) {
 }
 
 void accel(int to_speed) {
-
+    for (int i = 0; i < to_speed; i = i + 0.2 * to_speed) {
+        goForwardTime(0.1, i);
+    }
 }
 
 void decel(int to_speed) {
-
+    for (int i = avg_speed; i < to_speed; i = i - 0.2 * to_speed) {
+        goForwardTime(0.1, i);
+    }
 }
---
+++ autonomous/system/vars.h
@@ -0,0 +1,1 @@
+int avg_speed = 0;
