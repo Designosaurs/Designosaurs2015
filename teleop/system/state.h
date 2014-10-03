58886e3d3e385599ac6cb652d5d805ca620dffa1
williamtdr <william@cboos.com>
2014-10-03 17:33:12 -0500

    Create main loops

--- teleop/system/state.h
+++ teleop/system/state.h
@@ -1,3 +1,5 @@
+#include "globals.h"
+
 task UpdateDisplayTask();
 
 void onWait() {
@@ -28,6 +30,10 @@ void onRun() {
     nxtDisplayStringAt(20,42,"Designosaurs");
 
     nxtDisplayCenteredTextLine(5,"Running.");
+
+    while(systemState.running) {
+        wait1Msec(100);
+    }
 }
 
 void switchState(int newState) {
--- teleop/task/UpdateJoystickTask.h
+++ teleop/task/UpdateJoystickTask.h
@@ -1,13 +1,17 @@
 task UpdateJoystickTask() {
-    getJoystickSettings(joystick);
-    int y1 = joystick.joy1_y1;
-    int y2 = joystick.joy1_y2;
-    int x1 = joystick.joy1_x1;
-    int x2 = joystick.joy1_x2;
+    while(true) {
+        getJoystickSettings(joystick);
+        int y1 = joystick.joy1_y1;
+        int y2 = joystick.joy1_y2;
+        int x1 = joystick.joy1_x1;
+        int x2 = joystick.joy1_x2;
 
-    int PWR_LEFT = (0.78 * y1) * (DRIVE_POWER / 100);
-    int PWR_RIGHT = (0.78 * y2) * (DRIVE_POWER / 100);
+        int PWR_LEFT = (0.78 * y1) * (DRIVE_POWER / 100);
+        int PWR_RIGHT = (0.78 * y2) * (DRIVE_POWER / 100);
 
-    motor[left_drive] = PWR_LEFT;
-    motor[right_drive] = PWR_LEFT;
+        motor[left_drive] = PWR_LEFT;
+        motor[right_drive] = PWR_LEFT;
+
+        wait1Msec(50);
+    }
 }
