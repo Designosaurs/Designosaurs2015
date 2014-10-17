cef8948325ce4ca1d30998d421037db01954f61f
williamtdr <william@cboos.com>
2014-10-17 14:22:02 -0500

    Remove git changes

--- autonomous/task/MainTask.h
+++ autonomous/task/MainTask.h
@@ -10,26 +10,8 @@ task main() {
     StartTask(DebugTask);
     StartTask(UpdateEncoderTask);
 
-<<<<<<< HEAD
     goForwardDistance(6,80);
     goToTube();
-=======
-
-    // goForwardTime( 4, -80);
-
-    //accel(0,80);
-    //goForwardDistance(6,80);
-    //accel(80,0);
-
-
-    //accel(0,-80);
-    //goBackwardDistance(6,80);
-    //accel(-80,0);
-
-    pivotToTotalAngle( 90 );
 
     stop();
-while( 1 );
-    placeBall();
->>>>>>> 6b32cc62ef9adbe13b2c4d8834bb64c037ba50e0
 }
