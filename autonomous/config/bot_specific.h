2a509ec30b62a3defe9d4ad1276a0ae81c20aa6e
rteder <rteder@hydreon.com>
2014-10-19 09:54:26 -0500

    Goal place works well

--- autonomous/config/bot_specific.h
+++ autonomous/config/bot_specific.h
@@ -5,6 +5,6 @@ const float LEFT_TRIM = 1.000;
 const float RIGHT_TRIM = 1.000;
 const float LEFT_ENC_COMP = 0.960;
 
-const int GOAL_PLACE_DIST = 14;
+const float GOAL_PLACE_DIST = 13;
 const int GOAL_PLACE_SERVO_POS = 85;
 const int GOAL_PLACE_RAISE_POS = 200;
