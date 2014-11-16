1a5e26bd57bc524019ed9c7191e783d3627dca8a
williamtdr <william@cboos.com>
2014-11-16 15:08:12 -0600

    Fix trim values

--- autonomous/config/bot_specific.h
+++ autonomous/config/bot_specific.h
@@ -3,7 +3,7 @@ const int COUNTS_PER_FOOT = 1834;
 const float COUNTS_PER_DEGREE = 37.8;
 
 // If it veers left, use a smaller value here
-const float LEFT_ENC_COMP = 0.930;
+const float LEFT_ENC_COMP = 0.97;
 
 const float GOAL_PLACE_DIST = 12;
 const int GOAL_PLACE_SERVO_POS = 85;
