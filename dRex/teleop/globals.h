6cce341dff243b21c0703bef7790d862888cd7e3
williamtdr <william@cboos.com>
2014-12-15 19:19:11 -0600

    Fix merge conflicts

--- dRex/teleop/globals.h
+++ dRex/teleop/globals.h
@@ -2,12 +2,9 @@ float elbowPos;
 float wristPos;
 float lift_inches;
 bool liftStopped;
-<<<<<<< HEAD
 const int DEADBAND = 12;
 int drivePower = 100;
 int pwrLift;
 const int GUNNER_DEADBAND = 30;
-=======
 bool running_macro = false;
 bool override_macro = false;
->>>>>>> origin/master
