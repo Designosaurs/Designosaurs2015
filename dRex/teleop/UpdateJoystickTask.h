fc20db0a8d645403684e2914c5d0765ba195a04a
rteder <rteder@hydreon.com>
2014-12-16 18:45:55 -0600

    The Great directory compromise

--- dRex/teleop/UpdateJoystickTask.h
+++
@@ -1,8 +0,0 @@
-task UpdateJoystickTask() {
-    while(true) {
-        getJoystickSettings(joystick);
-        StartTask(DriverController);
-        StartTask(GunnerController);
-        wait1Msec(50);
-    }
-}
