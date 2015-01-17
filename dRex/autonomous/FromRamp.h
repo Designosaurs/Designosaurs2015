c2fbde7d75d3d309b002fb788dce13751fffd90f
William Teder <william@cboos.com>
2015-01-17 14:52:48 -0600

    Even more autonomus
    I mean what do I want to say? 5 commits and counting.

--- dRex/autonomous/FromRamp.h
+++ dRex/autonomous/FromRamp.h
@@ -2,16 +2,16 @@ void GoalFromRamp(){
     float angle_before_goal = 0;
 
     // Starting from top of ramp, go place the ball in the goal.
-    goForwardDistance(4.6, 40);
+    goForwardDistance(4.7, 40);
     DetectObstacles = 20;
-    if(goForwardDistance(2.1, 50)) IfBlocked();
+    if(goForwardDistance(2.2, 50)) IfBlocked();
     DetectObstacles = 0;
     stop();
     //stopAndWait();
     // Record the angle before we find the goal
     angle_before_goal = total_angle;
     GoalPlacer();
-    pivotDegrees(-8, 80); // Compensate for hardware being slightly off center
+    pivotDegrees(-3, 80); // Compensate for hardware being slightly off center
     jogForwardCm(17, 10);
     stop();
     goalGrabberDown();
