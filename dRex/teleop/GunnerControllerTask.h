99bcd151e1b7a68518aef2d09185ba535c947ec9
rteder <rteder@hydreon.com>
2015-01-23 19:30:13 -0600

    Slow down lift cont.

--- dRex/teleop/GunnerControllerTask.h
+++ dRex/teleop/GunnerControllerTask.h
@@ -12,7 +12,7 @@ task GunnerControllerTask {
 		if(abs(y1) > GUNNER_DEADBAND) {
 			if (y1 > 0)	y1 -= GUNNER_DEADBAND;
 			else y1 += GUNNER_DEADBAND;
-			pwrLift = y1 * 0.1; // Set max speed here.
+			pwrLift = y1 * 0.2; // Set max speed here.
 			motor[lift] = pwrLift;
 			} else {
 			motor[lift] = 0;
