28729df975c87235172e3e6b35b3b5da11105242
williamtdr <william@cboos.com>
2015-01-30 16:59:31 -0600

    Demo Mode

---
+++ dRex/tools/t_globals.h
@@ -0,0 +1,10 @@
+float elbowPos;
+float wristPos;
+float lift_inches;
+bool liftStopped;
+int drivePower = 100;
+// 0=Floor, 1 = HiGoal, 2 = CenterGoal
+int LiftPos = 0;
+int pwrLift;
+bool run_harvester = false;
+bool harv_back_jog = false;
