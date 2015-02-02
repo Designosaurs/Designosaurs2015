12f5819547bf111d8c2b94e4f8818532a5ad9801
rteder <rteder@hydreon.com>
2015-02-01 19:20:30 -0600

    Slop if blocked.

--- dRex/autonomous/FromRamp.h
+++ dRex/autonomous/FromRamp.h
@@ -4,14 +4,18 @@ void GoalFromRamp(){
     // Starting from top of ramp, go place the ball in the goal.
     goForwardDistance(4.7, 40);
     DetectObstacles = 20;
-    if(goForwardDistance(2.2, 50)) IfBlocked();
+    if(goForwardDistance(2.2, 50)) {
+    	// Blocked, so just stop, rather than risk damage.
+    	PlaySound(soundDownwardTones);
+    	stopAndWait();
+  	}
     DetectObstacles = 0;
     stop();
     //stopAndWait();
     // Record the angle before we find the goal
     angle_before_goal = total_angle;
     GoalPlacer();
-    pivotDegrees(-3, 80); // Compensate for hardware being slightly off center
+    //pivotDegrees(-3, 80); // Compensate for hardware being slightly off center
     jogForwardCm(17, 10);
     stop();
     goalGrabberDown();
