6e4d86bfd7529dad3605a94ba522e4613c918c2c
rteder <rteder@hydreon.com>
2015-02-04 19:34:48 -0600

    IR pointing routine works.

--- dRex/sensor/getIR.h
+++ dRex/sensor/getIR.h
@@ -1,4 +1,10 @@
 // Show each of the sensor readings.
+// IR Seeker outputs
+int dirAC = 0;
+int PeakIR = 0;
+int acS1, acS2, acS3, acS4, acS5 = 0;
+int Center6 = 0;  // Aim with respect to the center of direction 6.  Positive means to the right.
+
 void GetIR(void) {
 	// read the current modulated signal direction
 	dirAC = HTIRS2readACDir(HTIRS2);
@@ -18,4 +24,54 @@ void GetIR(void) {
 	Center6 = acS4 - acS3;
 	if (Center6 > 10) Center6 = 10;
 	if (Center6 < -10) Center6 = -10;
+
+	nxtDisplayTextLine(5, "IR: %d DIR: %d ", PeakIR, dirAC);
+
+}
+
+int Sign( int InVal) {
+	if (InVal >= 0) return 1;
+	else return -1;
+}
+
+////////////////////////////////////////////////////////////////
+// Points to direction 6 of the IR sensor.
+// The zones are of uneven widths, and direction 6 is one of the narrow ones.
+// The center of direction 6 is about 30 degrees to the right of straight.
+// So, in most applications the sensor will NOT be pointed straight at the target.
+// This function will pivot the robot until it points to the center of zone 6.
+// It will return true if it suceeds, false if it fails.
+// Must have PID control, because it puts very low speed values to the motors.
+
+bool PointToIR( void ) {
+	int PivotSpeed = 0;
+	int initialSign = 0;
+	int numTrys = 0;
+
+	GetIR();
+	if (PeakIR < 60) return false;
+	initialSign = Sign( Center6 );
+
+	while( true ){
+		GetIR();
+		PivotSpeed = Center6;
+		motor[right_drive] = -PivotSpeed;
+		motor[left_drive] = PivotSpeed;
+
+		// End when it is sraight on or just crosses sign.
+		if ((Sign( Center6) != initialSign)||( Center6 == 0)) {
+			motor[right_drive] = 0;
+			motor[left_drive] = 0;
+			return true;
+		}
+
+		if (PeakIR < 60) {
+			motor[right_drive] = 0;
+			motor[left_drive] = 0;
+			return false;
+		}
+		if (numTrys++ > 100) return false;
+
+		wait1Msec( 50 );
+	}
 }
